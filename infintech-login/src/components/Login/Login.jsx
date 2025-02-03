498b96f1763eaf2413d8eb4e7f9001d78ae7a994
Ajeeth Thangarasu <ajeetht21@gmail.com>
2025-02-03 18:26:45 +0530

    integarted api

--- infintech-login/src/components/Login/Login.jsx
+++ infintech-login/src/components/Login/Login.jsx
@@ -1,41 +1,77 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuth } from './AuthContext';
-import '../../components/Global.css';
-import logo from '../../images/download.jpg';
-import backgroundImage from '../../images/background_pictures.jpg';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./AuthContext";
+import "../../components/Global.css";
+import logo from "../../images/download.jpg";
+import backgroundImage from "../../images/background_pictures.jpg";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState(''); // State for error message
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState(""); // State for error message
   const navigate = useNavigate();
   const { login } = useAuth();
 
-  const handleSubmit = (e) => {
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   setLoading(true);
+  //   setMessage('Verifying credentials...');
+
+  //   const user = JSON.parse(localStorage.getItem('user'));
+
+  //   // Check if credentials match the saved user in localStorage
+  //   setTimeout(() => {
+  //     if (user && user.email === email && user.password === password) {
+  //       setMessage('Login successful!');
+  //       setErrorMessage(''); // Clear error message
+  //       login();
+  //       setTimeout(() => {
+  //         setLoading(false);
+  //         navigate('/');
+  //       }, 0);
+  //     } else {
+  //       setErrorMessage('Invalid credentials'); // Set error message if credentials are invalid
+  //       setLoading(false);
+  //     }
+  //   }, 0); // Simulate login delay
+  // };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    setMessage('Verifying credentials...');
-
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    // Check if credentials match the saved user in localStorage
-    setTimeout(() => {
-      if (user && user.email === email && user.password === password) {
-        setMessage('Login successful!');
-        setErrorMessage(''); // Clear error message
-        login();
-        setTimeout(() => {
+    await fetch("https://localhost:7097/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.isSuccess) {
+          sessionStorage.setItem("jwt", data.message);
+          console.log(data.message);
+          login();
+          setTimeout(() => {
+            setLoading(false);
+            navigate("/");
+          }, 0);
+        } else if (
+          data.isSuccess === false &&
+          data.message === "User credentials wrong"
+        ) {
+          alert("User credentials wrong");
           setLoading(false);
-          navigate('/');
-        }, 0);
-      } else {
-        setErrorMessage('Invalid credentials'); // Set error message if credentials are invalid
-        setLoading(false);
-      }
-    }, 0); // Simulate login delay
+        } else {
+          alert("Error in login");
+          setLoading(false);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
   };
 
   return (
@@ -67,20 +103,24 @@ const Login = () => {
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-          <button type="submit" className="login-button">LOGIN</button>
+          <button type="submit" className="login-button">
+            LOGIN
+          </button>
         </form>
         {/* Error message displayed below the login button */}
         {errorMessage && <div className="error-message">{errorMessage}</div>}
 
         <div className="login-links">
           <a href="/forgot-password">FORGOT YOUR PASSWORD?</a>
-          <span>Don't Have An Account? <a href="/register">SIGN UP</a></span>
+          <span>
+            Don't Have An Account? <a href="/register">SIGN UP</a>
+          </span>
         </div>
 
         {/* University Sign-Up Section */}
         <div className="university-signup">
-      <a href="/register-university">Sign Up with University</a>
-</div>
+          <a href="/register-university">Sign Up with University</a>
+        </div>
       </div>
       <div className="login-image">
         <img src={backgroundImage} alt="Background" />
--- infintech-login/src/index.js
+++ infintech-login/src/index.js
@@ -1,12 +1,12 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from  "react-dom/client";
 import App from './App';
 import './index.css'; // Global CSS styles if any
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
